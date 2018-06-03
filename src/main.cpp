e9285897e564ff5aa124bd0d2908bc794814b2a1
Rohan <rohanjudd@gmail.com>
2018-06-03 10:25:13 +0100

    debug message change

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@
 XT_Wav_Class ForceWithYou(Force);     // create an object of type XT_Wav_Class that is used by
                                       // the dac audio class (below), passing wav data as parameter.
 
-XT_DAC_Audio_Class DacAudio(25,0);    // Create the main player class object.
+XT_DAC_Audio_Class DacAudio(A0,0);    // Create the main player class object.
                                       // Use GPIO 25, one of the 2 DAC pins and timer 0
 
 void setup() {
@@ -23,8 +23,11 @@ void setup() {
 
 void loop() {
   static uint32_t i=0;                // simple counter to output
-  if(ForceWithYou.Completed)          // if completed playing, play again
-    DacAudio.PlayWav(&ForceWithYou);  // play the wav (pass the wav class object created at top of code
+  if(ForceWithYou.Completed)
+  {         // if completed playing, play again
+    i++;                                // increment the value of i
+    DacAudio.PlayWav(&ForceWithYou);
+  } // play the wav (pass the wav class object created at top of code
   Serial.println(i);                  // print out the value of i
-  i++;                                // increment the value of i
+
 }
